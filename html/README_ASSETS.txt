c0df0ffc142a77979bc09b0efa82c7dd3f148b7e
DoYoung Ryu <dy8327@gmail.com>
2026-05-22 11:50:23 +0900

    add:sample

---
+++ html/README_ASSETS.txt
@@ -0,0 +1,13 @@
+COSMIC MALL 이미지 에셋 안내
+
+추가된 이미지:
+- images/space_hero.svg      : 메인 상단 우주/무지개 배경
+- images/cosmic_banner.svg   : 하단 배너용 행성 이미지
+- images/aurora_card.svg     : 상품 카드 임시 오로라 이미지
+
+적용 위치:
+- css/style.css 하단의 'Cosmic image asset overrides' 영역에서 적용됨
+
+JSP로 옮길 때 경로 예시:
+<link rel="stylesheet" href="./css/style.css">
+<img src="./images/aurora_card.svg">
---
+++ html/js/product.js
@@ -0,0 +1,4 @@
+function openConfirm(){document.getElementById('confirmModal').classList.add('show')}
+function closeConfirm(){document.getElementById('confirmModal').classList.remove('show')}
+function showDone(){closeConfirm();document.getElementById('doneModal').classList.add('show')}
+function closeDone(){document.getElementById('doneModal').classList.remove('show')}
